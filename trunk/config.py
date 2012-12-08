a4f909ba3c39c56536419f4e24e2e1482a491d55
gedolian@gmail.com <gedolian@gmail.com@296412d9-f275-486b-8848-8492143b9467>
2012-12-08 18:53:21 +0000

    commit v.0.0.3

--- trunk/config.py
+++ trunk/config.py
@@ -1,5 +1,35 @@
 '''
-configuration support
+config.py
+
+Zonewalk configuration support
+gsk dec 2012
+
+
+LICENSE:
+
+Copyright (c) <2012>, <gsk>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided 
+that the following conditions are met:
+
+    Redistributions of source code must retain the above copyright notice, this list of conditions 
+    and the following disclaimer.
+    Redistributions in binary form must reproduce the above copyright notice, this list of conditions 
+    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    Neither the name of the <ORGANIZATION> nor the names of its contributors may be used to endorse or 
+    promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
 '''
 
 import os
@@ -8,7 +38,8 @@ from filedialog import FileDialog
 
 class Configurator():
     
-    def __init__(self):
+    def __init__(self, world):
+        self.world = world
         self.cfg_file = None
         self.config = {}
         self.frmDialog = None
@@ -35,7 +66,7 @@ class Configurator():
                 value = tokens[1].strip()
                 
                 self.config[key] = value
-                print 'config: %s = %s' % (key, value)
+                self.world.consoleOut('config: %s = %s' % (key, value))
     
     def saveConfig(self):
         try:
@@ -52,7 +83,6 @@ class Configurator():
         
     def confPathCallback(self, path):
         if not os.path.exists(path):
-            print 'Error: invalid path'
             self.frmDialog.setStatus('Error, path invalid: '+path)
             return 0
 
