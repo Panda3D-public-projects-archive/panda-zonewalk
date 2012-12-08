5f60023fc8df69006bde8c31e58c39769b944903
gedolian@gmail.com <gedolian@gmail.com@296412d9-f275-486b-8848-8492143b9467>
2012-12-08 11:53:31 +0000

    Importing dev version 0.0.2 intro trunk

---
+++ trunk/wldfile.py
@@ -0,0 +1,267 @@
+'''
+Python .WLD file support
+gsk Dec 2012
+
+-------------------------------------------------------------------------------
+WLD REFERENCE:
+-------------------------------------------------------------------------------
+
+--------------------
+WLD File structure
+--------------------
+    HEADER              [ 7 * u32 dword = 28 bytes ]
+        MAGIC           [ 0x54503D02 ]
+        VERSION         [ 0x00015500 or  0x1000C800 ]
+        MAX_FRAGMENT    [ dword ]
+        UNKNOWN         [ dword ]
+        UNKNOWN         [ dword ]
+        NAMEHASHLEN     [ dword ]
+        UNKNOWN         [ dword ]
+    NAMEHASH            [ len = namehashlen ]
+    FRAGMENTS           [ n fragments with n = max_fragment + 1 ]
+
+
+--------------------
+Fragments reference
+--------------------
+
+Fragment types
+
+0x03 - Texture Bitmap Name(s)
+0x04 - Texture Bitmap Info
+0x05 - Texture Bitmap Info Reference
+0x06 - Two-dimensional Object
+0x07 - Two-dimensional Object Reference
+0x08 - Camera
+0x09 - Camera Reference
+0x10 - Skeleton Track Set
+0x11 - Skeleton Track Set Reference
+0x12 - Mob Skeleton Piece Track
+0x13 - Mob Skeleton Piece Track Reference
+0x14 - Static or Animated Model Reference/Player Info
+0x15 - Object Location
+0x16 - Zone Unknown
+0x17 - Polygon Animation?
+0x18 - Polygon Animation Reference?
+0x1B - Light Source
+0x1C - Light Source Reference
+0x21 - BSP Tree
+0x22 - BSP Region
+0x28 - Light Info
+0x29 - Region Flag
+0x2A - Ambient Light
+0x2C - Alternate Mesh
+0x2D - Mesh Reference
+0x2F - Mesh Animated Vertices Reference
+0x30 - Texture
+0x31 - Texture List
+0x32 - Vertex Color
+0x33 - Vertex Color Reference
+0x35 - First Fragment
+0x36 - Mesh
+0x37 - Mesh Animated Vertices
+
+Fragment Structures
+
+FRAGMENT
+    FRAGLEN             [ dword ]
+    FRAGTYPE            [ dword ]
+    FRAGNAME            [ dword ]
+    FRAGDATA            [ bytes, length = FRAGLEN -4 ]
+    
+FRAGMENTREFERENCE    
+    FRAGINDEX           [ dword, points into the wld fragments table ]
+    
+'''
+
+
+import struct, array
+
+Version1WLD = 0x00015500
+Version2WLD = 0x1000C800
+MagicWLD =  0x54503D02
+
+from fragment import *
+
+        
+class WLDFile():
+    
+    def __init__(self, name):
+        self.name = name
+        self.filename = name+'.wld'
+                
+        # xor codes for the simple hash encoder
+        self.codes = [0x95, 0x3A, 0xC5, 0x2A, 0x95, 0x7A, 0x95, 0x6A]
+        
+        self.names = None   # decoded namehash
+        self.fragment_decoders = { 
+            0x36 : self.decode0x36, 0x03 : self.decode0x03, 0x31 : self.decode0x31,
+            0x30 : self.decode0x30, 0x05 : self.decode0x05, 0x04 : self.decode0x04
+            }
+            
+        self.fragment_type_counts = {}
+        self.fragments = {}
+        
+    # this simple hash encoder/decoder is used for the contents of the names string table 
+    # (hence its designation as "namehash") and for several other types of data inside the wld
+    def decodeBytes(self, bytes):
+        for i in range(0, len(bytes)):
+            bytes[i] = bytes[i] ^ self.codes[i & 7] 
+            
+        return bytes
+            
+    def countFragmentType(self, type):
+        if (self.fragment_type_counts.has_key(type)):
+            self.fragment_type_counts[type] += 1
+        else:
+            self.fragment_type_counts[type] = 1
+          
+    # return a name string from the namehash bytearray
+    # nameidx is the position pointer to the start of the name, it's end is marked
+    # with a C style 0 byte
+    def getName(self, nameidx):
+        position = -nameidx
+        if position == 0:
+            return ''
+            
+        name = self.names[position:]
+        # find the position of 0 byte terminator
+        i = 0
+        end = 0
+        for c in name:
+            if c == 0:
+                end = i
+                break;
+            i += 1
+         
+        name = name[0:end]        
+        return str(name)
+        
+    # -------------------------------------------------------------------------
+    # FRAGMENT decoders    
+    # we pass in the fragment type and nameRef already decoded in the main fragment reading loop
+    # additionaly the original file offset of the fragment is used as fragment ID which 
+    # is useful for easy handling of fragment references
+    
+    def decode0x36(self, id, type, nameRef, buf, offset):
+        f = Fragment36(id, type, nameRef, self)
+        self.fragments[f.id] = f
+        f.decode(buf, offset)
+        # f.dump()
+    def decode0x31(self, id, type, nameRef, buf, offset):
+        f = Fragment31(id, type, nameRef, self)
+        self.fragments[f.id] = f
+        f.decode(buf, offset)
+        # f.dump()
+    def decode0x30(self, id, type, nameRef, buf, offset):
+        f = Fragment30(id, type, nameRef, self)
+        self.fragments[f.id] = f
+        f.decode(buf, offset)
+        # f.dump()
+    def decode0x05(self, id, type, nameRef, buf, offset):
+        f = Fragment05(id, type, nameRef, self)
+        self.fragments[f.id] = f
+        f.decode(buf, offset)
+        # f.dump()
+    def decode0x04(self, id, type, nameRef, buf, offset):
+        f = Fragment04(id, type, nameRef, self)
+        self.fragments[f.id] = f
+        f.decode(buf, offset)
+        # f.dump()
+    def decode0x03(self, id, type, nameRef, buf, offset):
+        f = Fragment03(id, type, nameRef, self)
+        self.fragments[f.id] = f
+        f.decode(buf, offset)
+        # f.dump()
+
+
+    # -------------------------------------------------------------------------
+    # main loader driver
+    def load(self, s3d):
+        print 'WLDFile loading ', self.filename, ' from S3D container'
+        
+        s3dfile =  s3d.getFile(self.filename)
+        wld = s3dfile.data
+        # print 'total length:', s3dfile.size
+        
+        # process WLD header
+        offset = 0
+        (magic, version, max_fragment, dummy1, dummy2, name_hash_len) = struct.unpack('<iiiiii', wld[offset:offset+24])
+        offset += 28    # header length = 7 * u32
+        
+        version &= 0xffffffffe
+        if (magic != MagicWLD):
+            print 'invalid file magic number, aborting load'
+            return
+            
+        # print 'WLD magic=0x%x version=0x%x max_fragment=%i' % (magic, version, max_fragment)
+        if (version == Version1WLD):
+            print 'processing version 1 WLD file'
+        elif (version == Version2WLD):
+            print 'processing version 2 WLD file'
+        else:
+            return
+        
+        self.version = version
+        
+        # process NAMEHASH
+        # print 'name_hash length:', name_hash_len
+        format = '<'+str(name_hash_len)+'s'
+        (name_hash,) = struct.unpack(format, wld[offset:offset+name_hash_len])
+        offset += name_hash_len
+        
+        self.names = self.decodeBytes(bytearray(name_hash))
+        # self.names = str(self.names).split('\0')
+        # print self.names
+        
+        # load the FRAGMENTS
+        sum_len = 0
+        frag_id = 0
+        for i in range(0, max_fragment):
+            # fragment header
+            (fragment_len, fragment_type, fragment_name) = struct.unpack('<iii', wld[offset:offset+12])
+            
+            # fnam = self.getName(fragment_name)
+            # print fnam, len(fnam)
+            # print 'fragment len: %i type: 0x%x name:%s' % (fragment_len, fragment_type, fnam )
+            
+            self.countFragmentType(fragment_type)       # keep some statistics on fragment type counts
+
+            if self.fragment_decoders.has_key(fragment_type):
+                func = self.fragment_decoders[fragment_type]
+                func(frag_id, fragment_type, fragment_name, wld, offset)
+                    
+            # fragment data
+            sum_len += fragment_len + 8 # add the len and type fields (but not the name field, see below)
+            offset += 12                # skip header
+            offset += fragment_len-4    # for now simply skip, the fragment length seems to include the name field
+                                        # thus we need to subtract 4 
+            frag_id += 1                # fragment id is simply its position in the file
+        
+        # print 'sum of all fragment lengths:', sum_len
+        '''
+        for k in self.fragment_type_counts.keys():
+            print 'fragment type:0x%x count:%i' % (k, self.fragment_type_counts[k])
+        '''
+        print 'WLDFile load complete.'
+        print 'name0:', self.getName(-1)
+    
+    def getFragment(self, idx_plus_1):
+        # Note on fragment references
+        # references > 0 are a straight index into our fragments table 
+        # references <= 0 are a lot more involved: these have to be converted into
+        # namehash references like this: name_idx = (-frag_ref) - 1
+        # with this namehash ref we can then lookup the fragment name 
+        # finally we need to look through all our fragments to find one with matches the name
+        # whoever invented this nonsense should be bitch slapped silly
+        if idx_plus_1 > 0:
+            return self.fragments[idx_plus_1 - 1]
+        
+        nameRef = (idx_plus_1)-1
+        name = self.getName(nameRef)   # getName() expects a negated index, so we can pass as is
+        # print 'named frag reference:', name
+        for frag in self.fragments.values():
+            if frag.nameRef == nameRef:
+                return frag
+        
+        return None
---
+++ trunk/zone.py
@@ -0,0 +1,403 @@
+import struct
+import zlib
+
+from panda3d.core import Geom, GeomVertexData, GeomVertexFormat, GeomVertexWriter, GeomTriangles, GeomNode, CullFaceAttrib
+from panda3d.core import PNMImage, Texture, StringStream
+from panda3d.core import PandaNode, NodePath
+
+from s3dfile import S3DFile
+from wldfile import WLDFile
+from ddsfile import DDSFile
+
+
+# The PolyGroup defines a submesh in which all polygons use the same texture
+# Each PolyGroup is mapped directly to a Panda3D NodePath which gets the texture assigned
+class PolyGroup():
+    
+    # vdata is the Panda3D vertex data object created by the main Mesh
+    def __init__(self, vdata):
+        self.vdata = vdata      # vertex data, passed in from parent mesh
+        
+        self.name = 'blah'
+        self.geom = Geom(self.vdata)
+        self.primitives = GeomTriangles(Geom.UHStatic)
+        self.geom.addPrimitive(self.primitives)
+        
+        
+    def build(self, zone, f, start_index, n_polys, tex_idx):
+        polyList = f.polyList
+        poly_idx = start_index
+        for poly in range(0, n_polys):
+            p = polyList[poly_idx]
+            poly_idx += 1
+            self.primitives.addVertices(p[0], p[1], p[2])
+
+        self.node = GeomNode(self.name)
+        self.node.addGeom(self.geom)
+        # attach all our nodes under the zone's geometry root node
+        self.nodePath = zone.rootNode.attachNewNode(self.node)
+        
+        # self.nodePath.setRenderModeWireframe()
+        # Texture setup
+        # self.nodePath.setRenderModeFilled()
+        # self.nodePath.showBounds()
+        
+        self.nodePath.setPos(f.centerX, f.centerY,f.centerZ)    # translate to correct world position
+        self.nodePath.setAttrib(CullFaceAttrib.make(CullFaceAttrib.MCullCounterClockwise))
+
+        # Texture setup
+        sprite = zone.getSprite(tex_idx)
+        if sprite != None:
+            # sprite.dump()
+            t = sprite.textures[0]
+            self.nodePath.setTexture(t)
+        else:
+            pass
+            # print 'Error: texture (idx=%i) not found. PolyGroup will be rendered untextured' % (tex_idx)
+        
+         
+     
+        
+class Mesh():
+
+    def __init__(self, name):
+        self.name = name
+        self.poly_groups = []
+        
+        # GeomVertexFormats
+        #
+        # GeomVertexFormat.getV3cpt2()  - vertex, color, uv
+        # GeomVertexFormat.getV3t2()    - vertex, uv
+        # GeomVertexFormat.getV3c4()    - vertex, color
+        # GeomVertexFormat.getV3n3t2()  - vertex, normal, uv
+        # GeomVertexFormat.getV3n3cpt2()- vertex, normal, rgba, uv
+        
+        self.vdata = GeomVertexData(name,GeomVertexFormat.getV3n3cpt2(), Geom.UHStatic)
+        
+        self.vertex = GeomVertexWriter(self.vdata, 'vertex')
+        self.vnormal = GeomVertexWriter(self.vdata, 'normal')
+        self.color = GeomVertexWriter(self.vdata, 'color')
+        self.texcoord = GeomVertexWriter(self.vdata, 'texcoord')
+        
+        
+    # f is a 0x36 mesh fragment (see fragment.py for reference)
+    def buildFromFragment(self, f, zone):
+        # write vertex coordinates
+        for v in f.vertexList:
+            self.vertex.addData3f(v[0], v[1], v[2])
+
+        # write vertex colors
+        for rgba in f.vertexColorsList:
+            self.color.addData1f(rgba)
+
+        # write vertex normals
+        for v in f.vertexNormalsList:
+            self.vnormal.addData3f(v[0], v[1], v[2])
+            
+        # write texture uv
+        for uv in f.uvList:
+            self.texcoord.addData2f(uv[0], uv[1])
+            
+        # Build PolyGroups
+        # each polyTexList is a tuple containing a polygon count and the texture index for those polys        
+        # we create a PolyGroup for each of these
+        # the PolyGroup creates all Polygons sharing a texture and adds them under a NodePath
+        poly_idx = 0
+        for pt in f.polyTexList:
+            pg = PolyGroup(self.vdata)
+            self.poly_groups.append(pg)
+            
+            n_polys = pt[0]
+            tex_idx = pt[1]
+            
+            # pass fragment so that it can access the SPRITES
+            pg.build(zone, f, poly_idx, n_polys, tex_idx)
+            poly_idx += n_polys            
+        
+        if poly_idx != f.polyCount or poly_idx != len(f.polyList):
+            print 'ERROR: polycount mismatch'
+            
+        
+class Sprite():
+    
+    def __init__(self, name, idx):
+        self.name = name
+        self.index = idx
+        self.numtex = 0
+        
+        self.current_texture = 0
+        
+        self.texnames = []
+        self.textures = []
+        
+    def addTexture(self, texname, texture):
+        self.texnames.append(texname)
+        self.textures.append(texture)
+        self.numtex += 1
+        
+    def dump(self):
+        print 'SPITE: %s index:%i numtex:%i' % (self.name, self.index, self.numtex)
+        for name in self.texnames:
+            print name
+
+    
+class Zone():
+
+    def __init__(self, world, name, basedir):
+        self.world = world
+        self.name = name
+        self.basedir = basedir
+        
+        self.world.consoleOut('zone initializing: '+self.name)
+        
+        self.textures = {}      # dictionary of panda3d Texure() objects, indexed by texture name
+        self.sprites = {}       # SPRITE objects (1-n multitexture groups), indexed by 0x31 fragment index number
+                                # as referenced in our meshes
+        self.meshes = []
+        
+        self.rootNode = NodePath(PandaNode("zone_root"))
+        self.rootNode.reparentTo(render)
+        
+    def update(self):
+        pass
+        
+    def getSprite(self, index):
+        if self.sprites.has_key(index):
+            return self.sprites[index]
+        
+        return None
+            
+    def prepareZoneMesh(self):
+        
+        # load the 0x36 bsp region fragments (sub meshes): all these meshes together
+        # make up the main zone geometry
+        for f in self.wldZone.fragments.values():
+            if f.type == 0x36:
+                # print 'adding fragment_36 to main zone mesh'
+                # f.dump()
+                m = Mesh(self.name)
+                m.buildFromFragment(f, self)
+                self.meshes.append(m)
+                    
+    # dump .bmp file header info
+    def dumpBMPInfo(self, bm):
+        (magic, size, dummy, offset) = struct.unpack('<2siii', bm[0:14])
+        print 'bmp magic:%s size:%i offset:%i' % (magic, size, offset)
+        
+        (biSize, biWidth, biHeight, biPlanes, biBitCount, biCompression, biSizeImage, dummy1, dummy2, biClrUsed, biClrImportant) = \
+        struct.unpack('<iIIhhiiIIii', bm[14:14+40])
+        print 'biSize:%i biWidth:%i biHeight:%i biPlanes:%i biBitcount:%i biCompression:%i biSizeImage:%i biClrUsed:%i biClrImportant:%i' % \
+        (biSize, biWidth, biHeight, biPlanes, biBitCount, biCompression, biSizeImage, biClrUsed, biClrImportant)
+        
+    # Panda3D's PNImage loader cant deal with bmp files that have limited size palettes
+    # it only can process full size palletes (1024 bytes)
+    # Here we patch up the Bitmap header of a bmp from a s3d file to prepare it for extending the palette
+    # to the full size.
+    # NOTE that we dont touch the data in the bitmap info header (biSizeImage and biClrUsed in particular)
+    # so in effect the headers we produce here are corrupt
+    # The PNImage loader accepts the files though (more proof that it doesnt even correctly evaluate the header
+    # but simply assumes a std 1024 bytes pallete (and thus the std. 14+40+1024 = 1078 bytes offset)
+
+    def patchBmHeader(self, bm, new_offset):
+        (magic, size, dummy, offset) = struct.unpack('<2siii', bm[0:14])       
+        (biSize, biWidth, biHeight, biPlanes, biBitCount, biCompression, biSizeImage, dummy1, dummy2, biClrUsed, biClrImportant) = \
+        struct.unpack('<iIIhhiiIIii', bm[14:14+40])
+
+        size_diff = offset - new_offset
+        new_size = size + size_diff
+
+        bm_hdr = struct.pack('<2siii', magic, new_size, dummy, new_offset)       
+        bm_info_hdr = struct.pack('<iIIhhiiIIii', biSize, biWidth, biHeight, biPlanes, biBitCount, biCompression, biSizeImage, dummy1, dummy2, biClrUsed, biClrImportant)
+        new_bm_hdr = bm_hdr + bm_info_hdr
+        self.dumpBMPInfo(new_bm_hdr)
+        return new_bm_hdr
+        
+    # some of the ancient texture files in the old s3d archives even use short color tables
+    # (not the standard 256*4 = 1024 bytes). Panda3D's PNImage loader barfs on these. 
+    # Patch them up here
+    def checkBmp(self, bm, name):
+        (magic, size, dummy, offset) = struct.unpack('<2siii', bm[0:14])
+        if offset != 1078:  # the "normal" offset for a palletized 8 bit image (1024 palette+54 header)
+            print 'Patching up Panda3D incompatible .bmp texture:', name
+    
+            color_table = bm[54:offset]     # extract original color table
+            image_data = bm[offset:size]    # extract original image data
+            
+            hdr = self.patchBmHeader(bm, 1078)  # fix up the bitmap header
+            new_bm = hdr + color_table + str(bytearray(1078-offset)) + image_data # assemble new bmp
+        else:
+            new_bm = bm     # leave it as is
+            
+        return new_bm
+        
+    # Parameter f is a 0x03 type texture file definition fragment
+    def loadTexture(self, f):
+        # Note that we reference the texture files by name. The loader here
+        # loads textures only if they are not already in the dictionary. 
+        
+        # this currently only supports the .bmp files I found in the old (pre GOD?) s3d zone files
+        texname = self.wldZone.getName(f.nameRef)
+        
+        # In typical windows style we'll encounter all types of mixed case names, even spelled
+        # differently in various fragments within the same file: Once again we simply enforce all lower case
+        # before using the name as a key
+        # This works by the assumption that texture names are unique 
+        # (and also case insensitive as s3d.getFile() enforces lower case on all names)
+        texname = texname.lower()
+        
+        if not self.textures.has_key(texname):
+            # print 'loading texture:', texname
+            s3dentry = self.s3d.getFile(texname+'.bmp')
+            if s3dentry == None:
+                s3dentry = self.s3d.getFile(texname+'.dds')
+                
+            if s3dentry != None:
+                texfile = s3dentry.data
+                (magic,) = struct.unpack('<2s', texfile[0:2])
+                if magic == 'BM':
+                    # Generic BMP file
+                    # self.dumpBMPInfo(texfile)
+                    texfile = self.checkBmp(texfile, texname)
+                    if texfile == None:
+                        return
+                    
+                    ts = StringStream(texfile)  # turn into an istream
+                    ti = PNMImage()             # create panda3d general purpose image object
+                    ti.read(ts)                 # load from istream
+                    
+                    t = Texture()               # create texture object
+                    t.load(ti)                  # load texture from pnmimage
+                    self.textures[texname] = t  # and finally store
+                elif magic == 'DD':
+                    # DDS file
+                    dds = DDSFile(texfile)
+                    dds.patchHeader()
+                    # dds.dumpHeader()
+                    # dds.uncompressToBmp()
+                    # dds.save(texname+'.dds')
+                    
+                    ts = StringStream(dds.buf)  # turn into an istream                   
+                    t = Texture()               # create texture object
+                    t.readDds(ts)               # load texture from dds ram image
+                    self.textures[texname] = t  # and finally store
+                else:
+                    print 'Error unsupported texture: %s magic:%s referenced in fragment: %i' % (texname, magic, f.id)
+            else:
+                print 'Error: texture %s not found in s3d archive' % (texname)
+            
+        
+    def prepTextureName(self, name):
+        n = name.lower()
+        i = name.find('.')
+        if i != -1:
+            n = n[0:i]
+            
+        return n
+        
+    def preloadTextures(self):
+        # loop over all 0x03 fragments and load all referenced texture files from the s3d
+        f31 = None
+        for f in self.wldZone.fragments.values():
+            if f.type == 0x03:
+                self.loadTexture(f)
+            if f.type == 0x31:
+                f31 = f         # we'll need this one below 
+                
+        # create the SPRITE objects: these can reference a single texture or
+        # a list of them (for animated textures like water, lava etc.)
+        # we need to step through all entries of the 0x31 list fragment for the zone
+        # We store the SPRITEs using their index within the 0x31 fragments list as the key
+        # because this is exactly how the meshes (0x36 fragments) reference them
+        idx = 0
+        for ref30 in f31.nameRefs:
+            sprite_error = 0
+            sprite = None
+            # print ref30
+            f30 = self.wldZone.getFragment(ref30)
+            material_name = self.wldZone.getName(f30.nameRef)
+            
+            # f30.dump()
+            # print 'looking up 0x05 fragment with id_plus_1:', f30.frag05Ref
+            
+            # Note that there are frag05Refs inside some 0x30 fragments with value <=0 
+            # these named references seem to point directly to 0x03 texture fragments
+            # instead of the usual indirection chain  0x05->0x04->0x03
+            # in some instances these point nowhere meaningful at all though. Need to catch all these
+            frag = self.wldZone.getFragment(f30.frag05Ref)
+            if frag != None:
+                if frag.type == 0x03:    # this is a direct 0x03 ref (see note above)
+                    f03 = frag
+                    texfile_name = self.prepTextureName(f03.names[0])
+                    # print texfile_name
+                    if self.textures.has_key(texfile_name):
+                        # we dont have a sprite def (0x04) for these, so we use the material (0x30) name
+                        sprite = Sprite(material_name, idx)
+                        sprite.addTexture(texfile_name, self.textures[texfile_name]) 
+                    else:
+                        sprite_error = 1
+                        print 'Error in Sprite:', material_name, 'Texure not found:', texfile_name                        
+                elif frag.type == 0x05:
+                    f05 = frag
+                    # f05.dump()
+                    f04 = self.wldZone.getFragment(f05.frag04Ref)
+                    # f04.dump()
+
+                    name = self.wldZone.getName(f04.nameRef)
+                    sprite = Sprite(name, idx)
+                    for f03ref in  f04.frag03Refs:
+                        f03 = self.wldZone.getFragment(f03ref)
+                        # f03.dump()
+                        # NOTE that this assumes the zone 0x03 fragments only ever reference one single texture
+                        texfile_name = self.prepTextureName(f03.names[0])
+                        # print texfile_name
+                        if self.textures.has_key(texfile_name):
+                            sprite.addTexture(texfile_name, self.textures[texfile_name]) 
+                        else:
+                            sprite_error = 1
+                            print 'Error in Sprite:', name, 'Texure not found:', texfile_name
+                else:
+                    sprite_error = 1
+                    print 'Error in Material:%s. Texture ref in 0x30 frag is not type 0x5 or 0x3 but 0x%x' % (material_name, frag.type)
+                    print 'F30 DUMP:'
+                    f30.dump()
+                    print 'Referenced Fragment DUMP:'
+                    frag.dump()
+                    
+            else:
+                sprite_error = 1
+                print 'Error in Sprite: could not resolve frag05ref:%i in 0x30 fragment:%i' % (f30.frag05Ref, f30.id)
+
+            if sprite_error != 1:   # only add error free sprites
+                # sprite.dump()
+                self.sprites[idx] = sprite
+                
+            idx += 1    # need to increment regardless of whether we stored or not
+                        # so that the index lookup using the refs in the 0x36's works
+                
+                
+    def load(self):
+        s3dfile_name = self.name+'.s3d'
+        self.world.consoleOut('zone loading zone s3dfile: ' + s3dfile_name)
+        
+        self.s3d = S3DFile(self.basedir+self.name)
+        if self.s3d.load() != 0:
+            self.world.consoleOut( 'ERROR loading s3dfile:' + self.basedir+s3dfile_name)
+            return -1
+            
+        # self.s3d.dumpListing()
+        
+        self.wldZone = WLDFile(self.name)
+        self.wldZone.load(self.s3d)
+
+        self.world.consoleOut('zone preloading textures')
+        self.preloadTextures()
+        self.world.consoleOut( 'zone preparing zone mesh')
+        self.prepareZoneMesh()
+
+        # let Panda3D attempt to flatten the zone geometry (reduce the excessive
+        # Geom count resulting from the layout of the .wld zone data as a huge
+        # bunch of tiny bsp regions)
+        self.world.consoleOut('flattening zone mesh geom tree')
+        self.rootNode.flattenStrong()    
+        
+        return 0
